745381bec393d94f86936823811e06fc9e8538e0
CHILUKA SUNNY <82013394+sunny-star@users.noreply.github.com>
2025-02-25 13:10:45 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -16,11 +16,11 @@ const HeroSection = () => {
     }
 
     return (
-        <div className='text-center'>
+        <div className='text-center bg-purple-200'>
             <div className='flex flex-col gap-5 my-10'>
                 <span className=' mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#334fce] font-medium'>No. 1 Job Hunt Website</span>
-                <h1 className='text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#6A38C2]'>Dream Jobs</span></h1>
-                <p>from JOB QUEST</p>
+                <h1 className='text-5xl font-bold'>Search, <br /> JOB <span className='text-[#ed3030]'>QUEST</span></h1>
+                <p>A smart job search platform designed to connect job seekers with the right opportunities effortlessly.</p>
                 <div className='flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
                     <input
                         type="text"
