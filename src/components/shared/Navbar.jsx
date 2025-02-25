6e8ae91f290595b57be838f1c64a95cd217f741f
CHILUKA SUNNY <82013394+sunny-star@users.noreply.github.com>
2025-02-25 13:07:25 +0530

    Update Navbar.jsx

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
         }
     }
     return (
-        <div className='bg-white'>
+        <div className='bg-gradient-to-r from-blue-100 to-indigo-800'>
             <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
                 <div>
                     <h1 className='text-2xl font-bold'>JOB<span className='text-[#F83002]'>QUEST</span></h1>
